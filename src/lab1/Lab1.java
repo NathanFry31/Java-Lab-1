c94cbe190264b94880e13eab42d9dc9c0d46dea8
Nathan <Nathan@LAPTOP-0CP0IGBE>
2017-01-18 14:23:11 -0500

    First Upload

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,20 @@
+
+package lab1;
+
+
+
+/**
+ *
+ * @author Nathan
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.print("Hello World. My GetHub Account Name is NathanFry31\n");
+    }
+    
+}
